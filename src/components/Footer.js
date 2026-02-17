b389bd7f1f420ea03930ca6a488509ab2d943237
Amanda Weerasooriya Arachchi <143576968+KasuniAmanda@users.noreply.github.com>
2026-02-17 23:28:14 +0530

    updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,6 @@
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import { MailchimpForm } from "./MailchimpForm";
 import logo from "../assets/img/logo.svg";
-import { Row, Col } from "react-bootstrap";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
@@ -11,19 +10,26 @@ export const Footer = () => {
         <footer className="footer">
             <Container>
                 <Row className="align-items-center">
-                    <MailchimpForm />
-                        <Col sm={6}>
-                            <img src={logo} alt="Logo"></img>
-                        </Col>
-                        <Col sm={6} className="text-center text-sm-end">
+
+                    {/* FIX: Wrap inside Col */}
+                    <Col sm={12}>
+                        <MailchimpForm />
+                    </Col>
+
+                    <Col sm={6}>
+                        <img src={logo} alt="Logo" />
+                    </Col>
+
+                    <Col sm={6} className="text-center text-sm-end">
                         <div className="social-icon">
-                            <a href=""><img src={navIcon1} /></a>
-                            <a href=""><img src={navIcon2} /></a>
-                            <a href=""><img src={navIcon3} /></a>
+                            <a href="#"><img src={navIcon1} alt="icon1" /></a>
+                            <a href="#"><img src={navIcon2} alt="icon2" /></a>
+                            <a href="#"><img src={navIcon3} alt="icon3" /></a>
                         </div>
                         <p>Copyright 2024. All Rights Reserved</p>
-                        </Col>
-                    </Row>
+                    </Col>
+
+                </Row>
             </Container>
         </footer>
     )
